0f708c73496b856cccfed38c70965d4e3c712285
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2013-03-24 16:08:28 +0100

    Cleanup

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 function WeatherCtrl($scope) {
-    $scope.zipcodes = [ 1000, 2700, 2650 ];
+    $scope.zipcodes = [ 2700 ];
 
     $scope.addZipcode = function(){
         $scope.zipcodes.push($scope.zipcode);
-    }
+    };
 
     $scope.removeZipcode = function(zipcode){
         var index = $scope.zipcodes.indexOf(zipcode);
         $scope.zipcodes.splice(index, 1);
-    }
+    };
 }
