2ebed0dd1b1abe3362c748231877752fc1e79a71
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2013-03-30 16:07:10 +0100

    All functionality in place, still ugly UI

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 function WeatherCtrl($scope) {
-    $scope.zipcodes = [ 2700 ];
+    $scope.zipcodes = loadZipcodes();
 
     $scope.addZipcode = function(){
         $scope.zipcodes.push($scope.zipcode);
+        $scope.zipcode = '';
     };
 
     $scope.removeZipcode = function(zipcode){
         var index = $scope.zipcodes.indexOf(zipcode);
         $scope.zipcodes.splice(index, 1);
     };
+
+    $scope.$watch('zipcodes', function(){
+        saveZipcodes();
+    }, true);
+
+    function saveZipcodes(){
+        localStorage["ZipCodes"] = $scope.zipcodes;
+    }
+    function loadZipcodes() {
+        var zipCodes = localStorage["ZipCodes"];
+        if (zipCodes === undefined) {
+            return [];
+        }
+        return zipCodes.split(",");
+    }
+
 }
